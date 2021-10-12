4b79fc0186e719e6422ff7f0cb3642393a3a601c
vukmir <ja004@seznam.cz>
2021-10-12 10:15:45 +0200

    4. Creating a Projectile
    https://docs.unrealengine.com/4.27/en-US/InteractiveExperiences/Networking/QuickStart/

--- Source/MP_try/ThirdPersonMPProjectile.h
+++ Source/MP_try/ThirdPersonMPProjectile.h
@@ -14,6 +14,30 @@ class MP_TRY_API AThirdPersonMPProjectile : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AThirdPersonMPProjectile();
+	
+	// Sphere component used to test collision.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class USphereComponent* SphereComponent;
+
+	// Static Mesh used to provide a visual representation of the object.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UStaticMeshComponent* StaticMesh;
+
+	// Movement component for handling projectile movement.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	// Particle used when the projectile impacts against another object and explodes.
+	UPROPERTY(EditAnywhere, Category = "Effects")
+	class UParticleSystem* ExplosionEffect;
+
+	//The damage type and damage that will be done by this projectile
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
+	TSubclassOf<class UDamageType> DamageType;
+
+	//The damage dealt by this projectile.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
+	float Damage;
 
 protected:
 	// Called when the game starts or when spawned
